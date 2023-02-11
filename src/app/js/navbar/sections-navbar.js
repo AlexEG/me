27618bc3dbd0d5f6a4a053543931108a1135f109
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-02-11 09:46:22 +0200

    sections navbar [DONE]

---
+++ src/app/js/navbar/sections-navbar.js
@@ -0,0 +1,36 @@
+const sectionNavLi1 = document.querySelector("#sectionNavLi1");
+const sectionNavLi2 = document.querySelector("#sectionNavLi2");
+const sectionNavLi3 = document.querySelector("#sectionNavLi3");
+const sectionNavLi4 = document.querySelector("#sectionNavLi4");
+
+const li1 = document.querySelector("#giveMeActive1");
+const li2 = document.querySelector("#giveMeActive2");
+const li3 = document.querySelector("#giveMeActive3");
+const li4 = document.querySelector("#giveMeActive4");
+
+function removeActiveNavbar() {
+  li1.classList.remove("active-section");
+  li2.classList.remove("active-section");
+  li3.classList.remove("active-section");
+  li4.classList.remove("active-section");
+}
+
+sectionNavLi1.addEventListener("click", function () {
+  removeActiveNavbar();
+  li1.classList.add("active-section");
+});
+
+sectionNavLi2.addEventListener("click", function () {
+  removeActiveNavbar();
+  li2.classList.add("active-section");
+});
+
+sectionNavLi3.addEventListener("click", function () {
+  removeActiveNavbar();
+  li3.classList.add("active-section");
+});
+
+sectionNavLi4.addEventListener("click", function () {
+  removeActiveNavbar();
+  li4.classList.add("active-section");
+});
