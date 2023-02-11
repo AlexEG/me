aaa887b549f5a9efefe6adb791de637535394337
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-02-11 05:12:19 +0200

    add DAY1 + 2&3 +JS linking   statistics-section
    statistics-section starting with wakatime

---
+++ src/app/js/sidebarDAYS/a-sidebar-const.js
@@ -0,0 +1,248 @@
+const day1 = document.querySelector("#day1");
+const day2 = document.querySelector("#day2");
+const day3 = document.querySelector("#day3");
+const day4 = document.querySelector("#day4");
+const day5 = document.querySelector("#day5");
+
+const day6 = document.querySelector("#day6");
+const day7 = document.querySelector("#day7");
+const day8 = document.querySelector("#day8");
+const day9 = document.querySelector("#day9");
+const day10 = document.querySelector("#day10");
+
+const day11 = document.querySelector("#day11");
+const day12 = document.querySelector("#day12");
+const day13 = document.querySelector("#day13");
+const day14 = document.querySelector("#day14");
+const day15 = document.querySelector("#day15");
+
+const day16 = document.querySelector("#day16");
+const day17 = document.querySelector("#day17");
+const day18 = document.querySelector("#day18");
+const day19 = document.querySelector("#day19");
+const day20 = document.querySelector("#day20");
+
+const day21 = document.querySelector("#day21");
+const day22 = document.querySelector("#day22");
+const day23 = document.querySelector("#day23");
+const day24 = document.querySelector("#day24");
+const day25 = document.querySelector("#day25");
+
+const day26 = document.querySelector("#day26");
+const day27 = document.querySelector("#day27");
+const day28 = document.querySelector("#day28");
+const day29 = document.querySelector("#day29");
+const day30 = document.querySelector("#day30");
+
+const day31 = document.querySelector("#day31");
+const day32 = document.querySelector("#day32");
+const day33 = document.querySelector("#day33");
+const day34 = document.querySelector("#day34");
+const day35 = document.querySelector("#day35");
+
+const day36 = document.querySelector("#day36");
+const day37 = document.querySelector("#day37");
+const day38 = document.querySelector("#day38");
+const day39 = document.querySelector("#day39");
+const day40 = document.querySelector("#day40");
+
+const day41 = document.querySelector("#day41");
+const day42 = document.querySelector("#day42");
+const day43 = document.querySelector("#day43");
+const day44 = document.querySelector("#day44");
+const day45 = document.querySelector("#day45");
+
+const day46 = document.querySelector("#day46");
+const day47 = document.querySelector("#day47");
+const day48 = document.querySelector("#day48");
+const day49 = document.querySelector("#day49");
+const day50 = document.querySelector("#day50");
+
+const day51 = document.querySelector("#day51");
+const day52 = document.querySelector("#day52");
+const day53 = document.querySelector("#day53");
+const day54 = document.querySelector("#day54");
+const day55 = document.querySelector("#day55");
+
+const day56 = document.querySelector("#day56");
+const day57 = document.querySelector("#day57");
+const day58 = document.querySelector("#day58");
+const day59 = document.querySelector("#day59");
+const day60 = document.querySelector("#day60");
+
+const day61 = document.querySelector("#day61");
+const day62 = document.querySelector("#day62");
+const day63 = document.querySelector("#day63");
+const day64 = document.querySelector("#day64");
+const day65 = document.querySelector("#day65");
+
+const day66 = document.querySelector("#day66");
+const day67 = document.querySelector("#day67");
+const day68 = document.querySelector("#day68");
+const day69 = document.querySelector("#day69");
+const day70 = document.querySelector("#day70");
+
+const day71 = document.querySelector("#day71");
+const day72 = document.querySelector("#day72");
+const day73 = document.querySelector("#day73");
+const day74 = document.querySelector("#day74");
+const day75 = document.querySelector("#day75");
+
+const day76 = document.querySelector("#day76");
+const day77 = document.querySelector("#day77");
+const day78 = document.querySelector("#day78");
+const day79 = document.querySelector("#day79");
+const day80 = document.querySelector("#day80");
+
+const day81 = document.querySelector("#day81");
+const day82 = document.querySelector("#day82");
+const day83 = document.querySelector("#day83");
+const day84 = document.querySelector("#day84");
+const day85 = document.querySelector("#day85");
+const day86 = document.querySelector("#day86");
+const day87 = document.querySelector("#day87");
+const day88 = document.querySelector("#day88");
+const day89 = document.querySelector("#day89");
+const day90 = document.querySelector("#day90");
+
+const day91 = document.querySelector("#day91");
+const day92 = document.querySelector("#day92");
+const day93 = document.querySelector("#day93");
+const day94 = document.querySelector("#day94");
+const day95 = document.querySelector("#day95");
+
+const day96 = document.querySelector("#day96");
+const day97 = document.querySelector("#day97");
+const day98 = document.querySelector("#day98");
+const day99 = document.querySelector("#day99");
+const day100 = document.querySelector("#day100");
+
+/*
+  aside ↓↓
+        ==> [div] .days-container
+              ↓↓
+              ==> [div] .day [#day100]
+                    ↓↓
+                    ==> [div] .day-number [#d1]
+                          
+  */
+
+const d1 = document.querySelector("#d1");
+const d2 = document.querySelector("#d2");
+const d3 = document.querySelector("#d3");
+const d4 = document.querySelector("#d4");
+const d5 = document.querySelector("#d5");
+
+const d6 = document.querySelector("#d6");
+const d7 = document.querySelector("#d7");
+const d8 = document.querySelector("#d8");
+const d9 = document.querySelector("#d9");
+const d10 = document.querySelector("#d10");
+
+const d11 = document.querySelector("#d11");
+const d12 = document.querySelector("#d12");
+const d13 = document.querySelector("#d13");
+const d14 = document.querySelector("#d14");
+const d15 = document.querySelector("#d15");
+
+const d16 = document.querySelector("#d16");
+const d17 = document.querySelector("#d17");
+const d18 = document.querySelector("#d18");
+const d19 = document.querySelector("#d19");
+const d20 = document.querySelector("#d20");
+
+const d21 = document.querySelector("#d21");
+const d22 = document.querySelector("#d22");
+const d23 = document.querySelector("#d23");
+const d24 = document.querySelector("#d24");
+const d25 = document.querySelector("#d25");
+
+const d26 = document.querySelector("#d26");
+const d27 = document.querySelector("#d27");
+const d28 = document.querySelector("#d28");
+const d29 = document.querySelector("#d29");
+const d30 = document.querySelector("#d30");
+
+const d31 = document.querySelector("#d31");
+const d32 = document.querySelector("#d32");
+const d33 = document.querySelector("#d33");
+const d34 = document.querySelector("#d34");
+const d35 = document.querySelector("#d35");
+
+const d36 = document.querySelector("#d36");
+const d37 = document.querySelector("#d37");
+const d38 = document.querySelector("#d38");
+const d39 = document.querySelector("#d39");
+const d40 = document.querySelector("#d40");
+
+const d41 = document.querySelector("#d41");
+const d42 = document.querySelector("#d42");
+const d43 = document.querySelector("#d43");
+const d44 = document.querySelector("#d44");
+const d45 = document.querySelector("#d45");
+
+const d46 = document.querySelector("#d46");
+const d47 = document.querySelector("#d47");
+const d48 = document.querySelector("#d48");
+const d49 = document.querySelector("#d49");
+const d50 = document.querySelector("#d50");
+
+const d51 = document.querySelector("#d51");
+const d52 = document.querySelector("#d52");
+const d53 = document.querySelector("#d53");
+const d54 = document.querySelector("#d54");
+const d55 = document.querySelector("#d55");
+
+const d56 = document.querySelector("#d56");
+const d57 = document.querySelector("#d57");
+const d58 = document.querySelector("#d58");
+const d59 = document.querySelector("#d59");
+const d60 = document.querySelector("#d60");
+
+const d61 = document.querySelector("#d61");
+const d62 = document.querySelector("#d62");
+const d63 = document.querySelector("#d63");
+const d64 = document.querySelector("#d64");
+const d65 = document.querySelector("#d65");
+
+const d66 = document.querySelector("#d66");
+const d67 = document.querySelector("#d67");
+const d68 = document.querySelector("#d68");
+const d69 = document.querySelector("#d69");
+const d70 = document.querySelector("#d70");
+
+const d71 = document.querySelector("#d71");
+const d72 = document.querySelector("#d72");
+const d73 = document.querySelector("#d73");
+const d74 = document.querySelector("#d74");
+const d75 = document.querySelector("#d75");
+
+const d76 = document.querySelector("#d76");
+const d77 = document.querySelector("#d77");
+const d78 = document.querySelector("#d78");
+const d79 = document.querySelector("#d79");
+const d80 = document.querySelector("#d80");
+
+const d81 = document.querySelector("#d81");
+const d82 = document.querySelector("#d82");
+const d83 = document.querySelector("#d83");
+const d84 = document.querySelector("#d84");
+const d85 = document.querySelector("#d85");
+
+const d86 = document.querySelector("#d86");
+const d87 = document.querySelector("#d87");
+const d88 = document.querySelector("#d88");
+const d89 = document.querySelector("#d89");
+const d90 = document.querySelector("#d90");
+
+const d91 = document.querySelector("#d91");
+const d92 = document.querySelector("#d92");
+const d93 = document.querySelector("#d93");
+const d94 = document.querySelector("#d94");
+const d95 = document.querySelector("#d95");
+
+const d96 = document.querySelector("#d96");
+const d97 = document.querySelector("#d97");
+const d98 = document.querySelector("#d98");
+const d99 = document.querySelector("#d99");
+const d100 = document.querySelector("#d100");
---
+++ src/app/js/sidebarDAYS/sidebar-days-links.js
@@ -0,0 +1,9 @@
+day1.addEventListener("click", function () {
+  location.href = "/html/day1.html";
+});
+day2.addEventListener("click", function () {
+  location.href = "/html/day2.html";
+});
+day3.addEventListener("click", function () {
+  location.href = "/html/day3.html";
+});
