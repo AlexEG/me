35aa3bb05bfaffb4a616a8dd3ca1a50594b9d07e
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-02-15 15:16:08 +0200

    2023-2-15

---
+++ src/app/js/sections/resources-YT-vid.js
@@ -0,0 +1,30 @@
+// // this is the thumbnail the real image No play on hover
+// let videoThumbnail = document.querySelectorAll(".video-thumbnail");
+
+// // this is the hidden GIF play on hover = display: block;
+
+// // a class to remove every play-on-hover form the hidden GIF = display:none;
+// // let hideYTgif = document.querySelectorAll(".hide-gif-from-every-yt-vid"); //22
+
+// /////////////////////////////////////////
+
+// // when hovring on <img class="video-thumbnail"> main thumbnail
+// // give <img class="thumbnail-gif"> class "play-on-hover" = display: block;
+// let thumbnailgif = document.querySelectorAll(".thumbnail-gif");
+
+// function addPlayOnHoverClasss() {
+//   console.log("add play-on-hover class form every GIF = display:block;");
+// }
+
+// let addPlayOnHover = () => {
+//   addPlayOnHoverClasss();
+// };
+
+// videoThumbnail.forEach(function (elem) {
+//   elem.addEventListener("click", function () {
+//     d2.classList.add("active");
+//   });
+// });
+
+// when hovring on <?? class="video-thumbnail"> main thumbnail
+// give a class to some elements to remover ".play-on-hover" from every video <img class="thumbnail-gif">
