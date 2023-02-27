7fcf7bd3633b69857bd01a860c259e988d19e0ae
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-02-28 01:45:36 +0200

    update?

--- src/app/js/DAYS-Resources/day16/podcast16.js
+++ src/app/js/DAYS-Resources/day16/podcast16.js
@@ -1,27 +1,13 @@
 function renderAllpodcastDarknetDAY16() {
   // (ep, source, epName, date, description)
   let episode1 = podcastDarknetTemplate(
-    `61`,
-    `https://dcs.megaphone.fm/ADV3775923435.mp3?key=56573c81cdb1ea546c55b90935cc5148&request_event_id=2a3f62b3-3d63-4fca-88ea-1d485e4c48b6`,
+    `64`,
+    `https://dcs.megaphone.fm/ADV9747880751.mp3?key=562d2c1e50d731ef239f44879fcdffce&request_event_id=cf8617db-23d9-48f2-9649-a506b9b95808`,
     `SAMY`,
-    `Mar 17, 2020`,
-    `Samy Kamkar is a hacker. And while he’s done a lot of stuff, he’s best known for creating the Samy Worm. Which spread its way through a popular social media site and had crazy results.`
-  );
-  let episode2 = podcastDarknetTemplate(
-    `62`,
-    `https://dcs.megaphone.fm/ADV1459627606.mp3?key=7f876c5fb73e7b1d4a190b213c6f629d&request_event_id=7bf14ddb-61e9-4328-9b6c-16ac9e10b1a1`,
-    `CAM`,
-    `Mar 31, 2020`,
-    `Cam’s story is both a cautionary tale and inspirational at the same time. He’s been both an attacker and defender. And not the legal kind of attacker. He has caused half a million dollars in damages with his attacks...`
-  );
-  let episode3 = podcastDarknetTemplate(
-    `63`,
-    `https://dcs.megaphone.fm/ADV1459627606.mp3?key=7f876c5fb73e7b1d4a190b213c6f629d&request_event_id=7bf14ddb-61e9-4328-9b6c-16ac9e10b1a1`,
-    `W0RMER`,
-    `Apr 14, 2020`,
-    `The hacker named w0rmer was active within AnonOps. These are Anonymous Operations which often organize and wage attacks on websites or people often with the purpose of social justice. Eventually w0rmer joined...`
+    `Apr 28, 2020`,
+    `Vodafone Greece is the largest telecom provider in Greece. But in 2004 a scandal within the company would pin them to be top of the news cycle in Greece for weeks. Hackers got in the network. And what they were after took....`
   );
 
-  let renderAllTemplate = episode1 + episode2 + episode3;
+  let renderAllTemplate = episode1;
   podcastDarknet.innerHTML = renderAllTemplate;
 }
--- src/app/js/DAYS-Resources/day17/youtube17.js
+++ src/app/js/DAYS-Resources/day17/youtube17.js
@@ -1,11 +1,168 @@
-// function renderAllvideoTemplateDAY000() {
-// (link, gif, thumbnail, title, channel)
-//   let video1 = videoTemplate(`link`, `gif`, `thumbnail`, `title`, `channel`);
-//   let video2 = videoTemplate(`link`, `gif`, `thumbnail`, `title`, `channel`);
+function renderAllvideoTemplateDAY17() {
+  // (link, gif, thumbnail, title, channel)
 
-//   let renderAllTemplate = video1 + video2;
+  let video1 = videoTemplate(
+    `https://www.youtube.com/watch?v=ZtqBQ68cfJc&t=1829s`,
+    `gif`,
+    `https://i.ytimg.com/vi/ZtqBQ68cfJc/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAqCOpUXX03lJVboPkBMys1y7hXHQ`,
+    `The 50 Most Popular Linux & Terminal Commands - Full Course for Beginners`,
+    `FreeCodeCamp.org`
+  );
+  let video2 = videoTemplate(
+    `https://www.youtube.com/watch?v=1-5bIDFybDk&t=4s`,
+    `gif`,
+    `https://i.ytimg.com/vi/1-5bIDFybDk/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA771_--PUBKJYQAjTiGDH5iEQOTw`,
+    `#038 - Switch Statement`,
+    `Elzero Web School`
+  );
+  let video3 = videoTemplate(
+    `https://www.youtube.com/watch?v=UYOmHW-5vwQ&t=3s`,
+    `gif`,
+    `https://i.ytimg.com/vi/UYOmHW-5vwQ/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAYkUxysLXdZkPFWrwjWrTTqHheZw`,
+    `#039 - Switch And If Condition`,
+    `Elzero Web School`
+  );
+  let video4 = videoTemplate(
+    `https://www.youtube.com/watch?v=MLVJhya1CV0&t=2s`,
+    `gif`,
+    `https://i.ytimg.com/vi/MLVJhya1CV0/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCvLxsmZ1gaYrCkU2KugxdRwmOArA`,
+    `#040 - Array Big Introduction`,
+    `Elzero Web School`
+  );
+  let video5 = videoTemplate(
+    `https://www.youtube.com/watch?v=COMiJl7l6kw&t=6s`,
+    `gif`,
+    `https://i.ytimg.com/vi/COMiJl7l6kw/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC_vWQZ_tzYIB2srhhU5um2rYi8hg`,
+    `#041 - Using Length With Array`,
+    `Elzero Web School`
+  );
+  let video6 = videoTemplate(
+    `https://www.youtube.com/watch?v=VvPxe6qj5XY&t=4s`,
+    `gif`,
+    `https://i.ytimg.com/vi/VvPxe6qj5XY/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCeQpFvWlHA5DEqI5JE0Im33I7MYA`,
+    `#042 - Add And Remove From Array`,
+    `Elzero Web School`
+  );
+  let video7 = videoTemplate(
+    `https://www.youtube.com/watch?v=ss75DBXkZ4Y&t=2s`,
+    `gif`,
+    `https://i.ytimg.com/vi/ss75DBXkZ4Y/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD1Spw-BMeb8j7LtOlSNpPoDOWvxA`,
+    `#043 - Searching Array`,
+    `Elzero Web School`
+  );
+  let video8 = videoTemplate(
+    `https://www.youtube.com/watch?v=1hOTVQKYtNY&t=1s`,
+    `gif`,
+    `https://i.ytimg.com/vi/1hOTVQKYtNY/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBzXhkWv3ygJB_qdGVXHUGjlvA8_w`,
+    `#044 - Sorting Arraysy`,
+    `Elzero Web School`
+  );
+  let video9 = videoTemplate(
+    `https://www.youtube.com/watch?v=MLcZ-12YW0U&t=1s`,
+    `gif`,
+    `https://i.ytimg.com/vi/MLcZ-12YW0U/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCb-rXnFOE75frlw6QVDODaAvIDWQ`,
+    `#045 - Slicing Array`,
+    `Elzero Web School`
+  );
+  let video10 = videoTemplate(
+    `https://www.youtube.com/watch?v=gDgw2sobpdU&t=2s`,
+    `gif`,
+    `https://i.ytimg.com/vi/gDgw2sobpdU/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAOUZsSO26KpG9uqpT7SIxuDdHRIA`,
+    `#046 - Joining Arrays`,
+    `Elzero Web School`
+  );
+  let video11 = videoTemplate(
+    `https://www.youtube.com/watch?v=YSxfPXLfSQM`,
+    `gif`,
+    `https://i.ytimg.com/vi/YSxfPXLfSQM/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAnt14DjQTiby43kU-89v2gPjhMCA`,
+    `#047 - Array Challenge`,
+    `Elzero Web School`
+  );
+  let video12 = videoTemplate(
+    `https://www.youtube.com/watch?v=NnR4sYnnBoI&t=1s`,
+    `gif`,
+    `https://i.ytimg.com/vi/NnR4sYnnBoI/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCk2K10cnS5-mq7f8uAqNYzIGDIcg`,
+    `#048 - Loop - For And Concept Of Loop`,
+    `Elzero Web School`
+  );
+  let video13 = videoTemplate(
+    `https://www.youtube.com/watch?v=cgr2f_wmxmY`,
+    `gif`,
+    `https://i.ytimg.com/vi/cgr2f_wmxmY/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD_uoC3-jZcj0E_nmxOSuRKnoZVTg`,
+    `#049 - Looping On Sequences`,
+    `Elzero Web School`
+  );
+  let video14 = videoTemplate(
+    `https://www.youtube.com/watch?v=xPAk_2sed_M&t=2s`,
+    `gif`,
+    `https://i.ytimg.com/vi/xPAk_2sed_M/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBEZNRuMZP1qDn8DGlr0ZnrVmfXlg`,
+    `#050 - Nested Loops And Trainings`,
+    `Elzero Web School`
+  );
+  let video15 = videoTemplate(
+    `https://www.youtube.com/watch?v=Z3fF7MrBPXc&t=2s`,
+    `gif`,
+    `https://i.ytimg.com/vi/Z3fF7MrBPXc/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDaYtzVQfNyPYYxzZis4Lop7T4UWQ`,
+    `#051 - Loop Control - Break, Continue, Label`,
+    `Elzero Web School`
+  );
+  let video16 = videoTemplate(
+    `https://www.youtube.com/watch?v=2bK3u5qbzik&t=1s`,
+    `gif`,
+    `https://i.ytimg.com/vi/2bK3u5qbzik/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBJlpFuCQlI0aJr7s247P1EpubA5A`,
+    `#052 - Loop - For Advanced Example`,
+    `Elzero Web School`
+  );
+  let video17 = videoTemplate(
+    `https://www.youtube.com/watch?v=5kD2LeFKPzE&t=2s`,
+    `gif`,
+    `https://i.ytimg.com/vi/5kD2LeFKPzE/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA6fi4m8YMbOp2Z_pS0Puqo5DiKhg`,
+    `#053 - Practice - Add Products To Page`,
+    `Elzero Web School`
+  );
+  let video18 = videoTemplate(
+    `https://www.youtube.com/watch?v=CkTIcjLufCE&t=2s`,
+    `gif`,
+    `https://i.ytimg.com/vi/CkTIcjLufCE/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLARFuClpFDcZP19JO-oho4OAUNODA`,
+    `#054 - Loop - While`,
+    `Elzero Web School`
+  );
+  let video19 = videoTemplate(
+    `https://www.youtube.com/watch?v=STs3tI1kDzM&t=3s`,
+    `gif`,
+    `https://i.ytimg.com/vi/STs3tI1kDzM/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDiGtbz8yaUgl7p98uZdzsu0xIcEA`,
+    `#055 - Loop - Do While`,
+    `Elzero Web School`
+  );
+  let video20 = videoTemplate(
+    `https://www.youtube.com/watch?v=Db2QlMCFOIY`,
+    `gif`,
+    `https://i.ytimg.com/vi/Db2QlMCFOIY/hq720.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAqv6RtTrpZQVStw1BndpJRI4wWgQ`,
+    `#056 - Loop - Challenge`,
+    `Elzero Web School`
+  );
 
-//   YouTubeDay15.innerHTML = renderAllTemplate;
-// }
+  let renderAllTemplate =
+    video1 +
+    video2 +
+    video3 +
+    video4 +
+    video5 +
+    video6 +
+    video7 +
+    video8 +
+    video9 +
+    video10 +
+    video11 +
+    video12 +
+    video13 +
+    video14 +
+    video15 +
+    video16 +
+    video17 +
+    video18 +
+    video19 +
+    video20;
 
-// renderAllvideoTemplateDAY000();
+  YouTube.innerHTML = renderAllTemplate;
+}
